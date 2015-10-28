0d55f2722eac4c6ecb1e3ead4d2c762f8b6a6e16
root <root@static.148-251-70-116.clients.your-server.de>
2015-10-28 11:42:39 +0100

    Changes on tl_module.php

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,6 +1,36 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['redirecter'] = '{title_legend},name,headline,type;{config_legend},jumpTo;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'redirecter_protected';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['redirecter'] = '{title_legend},name,headline,type;{config_legend},jumpTo,redirecter_guests,redirecter_protected;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
-#$GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_guests'];
-#$GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_protected'];
+$GLOBALS['TL_DCA']['tl_module']['subpalettes']['redirecter_protected'] = 'redirecter_groups';
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_guests'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['guests'],
+	'exclude'                 => true,
+	'filter'                  => true,
+	'inputType'               => 'checkbox',
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_protected'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['protected'],
+	'exclude'                 => true,
+	'filter'                  => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_groups'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['groups'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'foreignKey'              => 'tl_member_group.name',
+	'eval'                    => array('mandatory'=>true, 'multiple'=>true),
+	'sql'                     => "blob NULL",
+	'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
+);
