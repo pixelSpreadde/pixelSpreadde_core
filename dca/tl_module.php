b0879b299ac3c8f774eea1f3d24ee1c248eabb10
root <root@static.148-251-70-116.clients.your-server.de>
2016-06-02 07:06:20 +0200

    Changed ModuleRedirect

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,9 +1,22 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'redirecter_protected';
-$GLOBALS['TL_DCA']['tl_module']['palettes']['redirecter'] = '{title_legend},name,headline,type;{config_legend},jumpTo,redirecter_guests,redirecter_protected;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'check_news';
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['redirecter'] = '{title_legend},name,headline,type;{news_legend},check_news;{config_legend},jumpTo,redirecter_guests,redirecter_protected;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 $GLOBALS['TL_DCA']['tl_module']['subpalettes']['redirecter_protected'] = 'redirecter_groups';
+$GLOBALS['TL_DCA']['tl_module']['subpalettes']['check_news'] = 'news_archives';
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['check_news'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['check_news'],
+	'exclude'                 => true,
+	'filter'                  => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['redirecter_guests'] = array
 (
--- modules/ModuleRedirecter.php
+++ modules/ModuleRedirecter.php
@@ -26,6 +26,22 @@ class ModuleRedirecter extends \Module
 
 	protected function compile()
 	{
+		if($this->check_news && FE_USER_LOGGED_IN) {
+			$this->Import('FrontendUser', 'Member');
+
+			$objMember = \MemberModel::findById($this->Member->id);
+			$arrArchives = deserialize($this->news_archives);
+
+			if(is_array($arrArchives)) {
+				$objNews = $this->Database->prepare("SELECT * FROM tl_news WHERE pid IN(" . implode(",", $arrArchives) . ") && tstamp > ? && published='1'")->execute($objMember->lastLogin);
+
+				if(!$objNews->count()) {
+					$objPage = \PageModel::findById($this->jumpTo);
+					\Controller::redirect(\Controller::generateFrontendUrl($objPage->row(), ''));
+				}
+			}
+		}
+
 		if((!FE_USER_LOGGED_IN && $this->redirecter_guests) OR (FE_USER_LOGGED_IN && $this->redirecter_protected))
 		{
 			$objPage = \PageModel::findById($this->jumpTo);
