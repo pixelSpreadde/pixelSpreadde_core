7b2b4f8620c2c6083937d79e1083f8721ee69ba0
Sascha Brandhoff <sascha.brandhoff@gmail.com>
2016-06-02 07:11:34 +0200

    Added Languages-Files

---
+++ languages/de/modules.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['FMD']['redirecter'] = array('Redirecter', '');
---
+++ languages/de/tl_module.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['check_news'] = array('Auf neue Nachrichten pr&uuml;fen', 'Soll gepr&uuml;ft werden ob es neue Nachrichten gibt die angezeigt werden sollen?');
